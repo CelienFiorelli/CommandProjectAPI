e1908f4fdc142a47038d6d319267733d377c21e0
Celien Fiorelli <celien.fiorelli@esqese.ucly.fr>
2023-06-12 17:23:51 +0200

    correction point de fidélité et endpoint de paiement

--- post/create_payment.js
+++ post/create_payment.js
@@ -4,19 +4,22 @@ const { tokenIsValid } = require("../utils/tokenHandler");
 const Order = require("../models/Order");
 const Token = require("../models/Token");
 const User = require("../models/User");
+const { Decimal128 } = require("mongodb");
 
 module.exports = {
     endpoint: "/payment",
     process: async (req, res) => {
-        if (!req.query.card || !req.query.amount || !req.query.expiration_card || !req.query.crypto || !req.query.token || !(await tokenIsValid(req.query.token))) return res.send({ status: 404});
+        if (!req.query.card || !req.query.fidelity || !req.query.expiration_card || !req.query.crypto || !req.query.token || !(await tokenIsValid(req.query.token))) return res.send({ status: 404});
         
         const user = (await Token.findOne({ token: req.query.token }).populate("user")).user;
-
+        
+        const items = (await ShoppingCart.find({user: user}).populate("product"));
+        const amount = parseFloat((parseFloat(items.reduce((partialSum, p) => partialSum + parseFloat(p.product.price.toJSON().$numberDecimal) * p.quantity, 0).toFixed(2)) - parseInt(req.query.fidelity) / 7).toFixed(2))
+        
         const payment = await Payment.create({
-            user: user, amount: req.query.amount, card: req.query.card, crypto: req.query.crypto, expiration_card: req.query.expiration_card
+            user: user, amount: amount, card: req.query.card, crypto: req.query.crypto, expiration_card: req.query.expiration_card
         })
 
-        const items = (await ShoppingCart.find({user: user}).populate("product"));
         for (const item of items) {
             for (let i = 0; i < item.quantity; i++) {
                 await Order.create({payment: payment, product: item.product})
@@ -24,7 +27,8 @@ module.exports = {
         }
         await ShoppingCart.deleteMany({user: user})
 
-        user.fidelity += Math.round(req.query.amount) * 2;
+        user.fidelity += Math.round(amount) * 2;
+        user.fidelity -= parseInt(req.query.fidelity);
         user.save()
         
         res.send({ status: 200 })
